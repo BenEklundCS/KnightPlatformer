d1d4e7247f43dd3034d4d7679f84c6f51da5b3f9
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2022-07-29 23:14:43 +0000

    implemented jumping

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ WINDOW_SIZE = (1200, 800)
 backgroundColor = 0, 60, 120
 
 # Load all relevant images
-player_image = pygame.image.load(os.path.join('Characters/knight/idle','idle_knight_1.png'))
+player_image = pygame.image.load(os.path.join('Characters/knight/idle', 'idle_knight_1.png'))
 floor_image = pygame.image.load('Tiles/floor_tile_3.png')
 brick_image = pygame.image.load('Tiles/brick_1.png')
 # Set width of tiles for generation 
@@ -36,13 +36,15 @@ game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0
             ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2'],
             ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2']]
 
+
 # Collision and movement functions
 def collision_test(rect, tiles):
-  hit_list = []
-  for tile in tiles:
-    if rect.colliderect(tile):
-      hit_list.append(tile)
-  return hit_list
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
 
 def move(rect, movement, tiles):
     collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
@@ -65,13 +67,16 @@ def move(rect, movement, tiles):
             rect.top = tile.bottom
             collision_types['top'] = True
     return rect, collision_types
+
+
 # Default movement
 moving_right = False
 moving_left = False
 
 # Player movement and hitboxes
 player_y_momentum = 0
-player_rect = pygame.Rect(50, 50 , player_image.get_width(), player_image.get_height())
+air_timer = 0
+player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
 
 while True:
 
@@ -81,49 +86,59 @@ while True:
     tile_rects = []
     y = 0
     for row in game_map:
-      x = 0
-      for tile in row:
-        if tile == '1':
-          display.blit(floor_image, (x * TILE_SIZE, y * TILE_SIZE))
-        if tile == '2':
-          display.blit(brick_image, (x * TILE_SIZE, y * TILE_SIZE))
-        if tile != '0':
-          tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-        x += 1
-      y += 1
+        x = 0
+        for tile in row:
+            if tile == '1':
+                display.blit(floor_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile == '2':
+                display.blit(brick_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
-    #player_location[1] += player_y_momentum
+    # player_location[1] += player_y_momentum
     # Player movement handling
     player_movement = [0, 0]
     if moving_right:
-      player_movement[0] += 2
+        player_movement[0] += 3
     if moving_left:
-      player_movement[0] -= 2
+        player_movement[0] -= 3
     player_movement[1] += player_y_momentum
     player_y_momentum += 0.2
     if player_y_momentum > 3:
-      player_y_momentum = 3
+        player_y_momentum = 3
     # Run move function to determine player_rect movement
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
+    if collisions['bottom']:
+        player_y_momentum = 0
+        air_timer = 0
+    else:
+        air_timer += 1
     # Display player/refresh
     display.blit(player_image, (player_rect.x, player_rect.y))
-  
+
     # Game event loop
     for event in pygame.event.get():  # event loop
         if event.type == pygame.QUIT:  # check for window quit
             pygame.quit()  # stop pygame
             sys.exit()  # stop script
+        # Player controls
         if event.type == KEYDOWN:  # check for key pressed
             if event.key == K_RIGHT:
                 moving_right = True
             if event.key == K_LEFT:
                 moving_left = True
+            if event.key == K_UP:
+                if air_timer < 6:
+                    player_y_momentum = -4
         if event.type == KEYUP:  # check for key released
             if event.key == K_RIGHT:
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
 
+    # Display draw and updates
     surf = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(surf, (0, 0))
     pygame.display.update()
@@ -132,4 +147,6 @@ while True:
 
 def main():
     pass
-main()            
+
+
+main()
